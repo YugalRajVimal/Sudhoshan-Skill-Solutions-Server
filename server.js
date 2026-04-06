e2260c1dceb05a66f15b36c4ca948cab35791bfc
Yugal Raj Vimal <yugalrajvimal@Yugals-MacBook-Air.local>
2026-04-06 16:09:54 +0530

    --

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '10mb' }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json({ limit: '10mb' }));
 
 const port = process.env.PORT || 8822;
 
